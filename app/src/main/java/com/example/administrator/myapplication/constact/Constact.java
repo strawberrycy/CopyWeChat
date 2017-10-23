7e3300c4190731d3b76efd8fc3249a309e01c313
strawberrycy <1635304947@qq.com>
2017-10-23 14:29:31 +0800

    2017.10.23

---
+++ app/src/main/java/com/example/administrator/myapplication/constact/Constact.java
@@ -0,0 +1,8 @@
+package com.example.administrator.myapplication.constact;
+
+/**
+ * Created by Administrator on 2017/9/30.
+ */
+
+public interface Constact {
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/utils/PhotoUtils.java
@@ -0,0 +1,8 @@
+package com.example.administrator.myapplication.utils;
+
+/**
+ * Created by Administrator on 2017/9/30.
+ */
+
+public class PhotoUtils {
+}
