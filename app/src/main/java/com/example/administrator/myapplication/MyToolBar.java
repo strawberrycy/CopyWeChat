954e683db6cee31a498e85783aacfa2197d30992
strawberrycy <1635304947@qq.com>
2017-10-25 17:18:47 +0800

    自定义toolbar

---
+++ app/src/main/java/com/example/administrator/myapplication/MyToolBar.java
@@ -0,0 +1,10 @@
+package com.example.administrator.myapplication;
+
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Administrator on 2017/10/25.
+ */
+
+public class MyToolBar extends Toolbar {
+}
